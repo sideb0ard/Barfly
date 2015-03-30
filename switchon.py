3f05b7250247e2c8e4d450cf5bb06cd8fc4bf6be
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2015-03-30 01:08:35 +0000

    on/off

--- switchon.py
+++ switchon.py
@@ -1,21 +1,28 @@
 import os
 import sys
 
+devicenum = 67
+device = '/sys/class/gpio/export'
 direction = '/sys/class/gpio/gpio67/direction'
 onoff = '/sys/class/gpio/gpio67/value'
 
-def status(file):
+
+def init():
+	initf = open(device, 'w')
+	initf.write(devicenum + '\n')
+
+
+def status():
 	dirf = open(direction, 'r')
 	dirv = dirf.read()
 	print 'Direction: {0}'.format(dirv)
-	onoffr = open(onoff, 'r')
-	onoffv = onoffr.read()
-	print 'OnOff: {0}'.format(onoffv)
+	onofff = open(onoff, 'r')
+        onoffv = onofff.read()
+	print 'Val: {0}'.format(onoffv)
 
-def onoff(val):
-	onofff = open(onoff, 'w')
-	
-
+def setval(val):
+        f = open(onoff, 'w')
+        f.write(val)
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
@@ -23,4 +30,6 @@ if __name__ == '__main__':
 		sys.exit(1)
 
 	if sys.argv[1] == 'status':
-		status(file)
+		status()
+        elif sys.argv[1] == 'set':
+                setval(sys.argv[2])
