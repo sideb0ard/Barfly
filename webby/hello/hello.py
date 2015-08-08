d3b862ba1a0ea770956170e64220d09984831a7c
linus Tovre <thorsten.sideb0ard@gmail.com>
2015-08-08 23:31:18 +0000

    hupdate

--- webby/hello/hello.py
+++ webby/hello/hello.py
@@ -4,7 +4,9 @@ from autobahn.twisted.util import sleep
 from autobahn.twisted.wamp import ApplicationSession
 from autobahn.wamp.exception import ApplicationError
 
+import os
 import subprocess
+import time
 
 
 class AppSession(ApplicationSession):
@@ -15,8 +17,7 @@ class AppSession(ApplicationSession):
 
         def drinkmotherfucker(msg):
             print("DrINK MUTHERFUCKER CALLED! {}".format(msg))
-            print(self.imageMagic())
-            return "DRUNK"
+            return(self.imageMagic())
 
         reg = yield self.register(drinkmotherfucker, 'com.barfly.drinkmotherfucker')
         print("procedure DRINKMOTHERFUCKER() registered: {}".format(reg))
@@ -25,4 +26,8 @@ class AppSession(ApplicationSession):
     def imageMagic(self):
         # return "called da magic.."
         # return subprocess.check_output(["echo", "Calling Da Magic!"])
-        return subprocess.check_output(["/usr/bin/streamer", "-o", "filezz.jpg", "-s", "480-180"])
+        # return os.getcwd()
+        ofilename = "filezzz" + str(int(time.time())) + ".jpeg"
+        ofile = "/usr/local/Barfly/webby/hello/web/fileoutput/" + ofilename
+        subprocess.check_output(["/usr/bin/streamer", "-o", ofile, "-s", "480x180"])
+        return ofilename
