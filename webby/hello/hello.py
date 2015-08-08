0e01980689a37410348ce094213c6728d4b87ccc
linus Tovre <thorsten.sideb0ard@gmail.com>
2015-08-08 22:39:36 +0000

    blah

--- webby/hello/hello.py
+++ webby/hello/hello.py
@@ -4,50 +4,25 @@ from autobahn.twisted.util import sleep
 from autobahn.twisted.wamp import ApplicationSession
 from autobahn.wamp.exception import ApplicationError
 
+import subprocess
+
 
 class AppSession(ApplicationSession):
 
     @inlineCallbacks
     def onJoin(self, details):
 
-        # SUBSCRIBE to a topic and receive events
-        #
-        def onhello(msg):
-            print("event for 'onhello' received: {}".format(msg))
-
-        sub = yield self.subscribe(onhello, 'com.barfly.onhello')
-        print("subscribed to topic 'onhello': {}".format(sub))
-
 
         def drinkmotherfucker(msg):
             print("DrINK MUTHERFUCKER CALLED! {}".format(msg))
+            print(self.imageMagic())
             return "DRUNK"
 
         reg = yield self.register(drinkmotherfucker, 'com.barfly.drinkmotherfucker')
         print("procedure DRINKMOTHERFUCKER() registered: {}".format(reg))
 
 
-
-        # PUBLISH and CALL every second .. forever
-        #
-        counter = 0
-        while True:
-
-            # PUBLISH an event
-            #
-            yield self.publish('com.barfly.oncounter', counter)
-            print("published to 'oncounter' with counter {}".format(counter))
-            counter += 1
-
-            # CALL a remote procedure
-            #
-            try:
-                res = yield self.call('com.barfly.mul2', counter, 3)
-                print("mul2() called with result: {}".format(res))
-            except ApplicationError as e:
-                # ignore errors due to the frontend not yet having
-                # registered the procedure we would like to call
-                if e.error != 'wamp.error.no_such_procedure':
-                    raise e
-
-            yield sleep(1)
+    def imageMagic(self):
+        # return "called da magic.."
+        # return subprocess.check_output(["echo", "Calling Da Magic!"])
+        return subprocess.check_output(["/usr/bin/streamer", "-o", "filezz.jpg", "-s", "480-180"])
