a9e0e841430ee626c5e825eccf62b0ca1770fc86
linus Tovre <thorsten.sideb0ard@gmail.com>
2015-08-08 21:44:22 +0000

    changing example to barfly

--- webby/hello/hello.py
+++ webby/hello/hello.py
@@ -1,31 +1,3 @@
-###############################################################################
-#
-# Copyright (C) 2014, Tavendo GmbH and/or collaborators. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-###############################################################################
-
 from twisted.internet.defer import inlineCallbacks
 
 from autobahn.twisted.util import sleep
@@ -43,17 +15,18 @@ class AppSession(ApplicationSession):
         def onhello(msg):
             print("event for 'onhello' received: {}".format(msg))
 
-        sub = yield self.subscribe(onhello, 'com.example.onhello')
+        sub = yield self.subscribe(onhello, 'com.barfly.onhello')
         print("subscribed to topic 'onhello': {}".format(sub))
 
-        # REGISTER a procedure for remote calling
-        #
-        def add2(x, y):
-            print("add2() called with {} and {}".format(x, y))
-            return x + y
 
-        reg = yield self.register(add2, 'com.example.add2')
-        print("procedure add2() registered: {}".format(reg))
+        def drinkmotherfucker(msg):
+            print("DrINK MUTHERFUCKER CALLED! {}".format(msg))
+            return "DRUNK"
+
+        reg = yield self.register(drinkmotherfucker, 'com.barfly.drinkmotherfucker')
+        print("procedure DRINKMOTHERFUCKER() registered: {}".format(reg))
+
+
 
         # PUBLISH and CALL every second .. forever
         #
@@ -62,14 +35,14 @@ class AppSession(ApplicationSession):
 
             # PUBLISH an event
             #
-            yield self.publish('com.example.oncounter', counter)
+            yield self.publish('com.barfly.oncounter', counter)
             print("published to 'oncounter' with counter {}".format(counter))
             counter += 1
 
             # CALL a remote procedure
             #
             try:
-                res = yield self.call('com.example.mul2', counter, 3)
+                res = yield self.call('com.barfly.mul2', counter, 3)
                 print("mul2() called with result: {}".format(res))
             except ApplicationError as e:
                 # ignore errors due to the frontend not yet having
